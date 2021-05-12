8dca4e5223bf9d85c32c078643720b7f1639092a
Felecarp <felecarp@suboxide.org>
2021-05-12 10:45:34 +0200

    Surface.set_colorkey only on parent surface

--- sprite_tutorial/spritesheet.py
+++ sprite_tutorial/spritesheet.py
@@ -4,9 +4,8 @@ class SpriteSheet():
 	def __init__(self, image):
 		self.sheet = image
 
-	def get_image(self, frame, width, height, scale, colour):
+	def get_image(self, frame, width, height, scale):
 		image = self.sheet.subsurface((frame * width, 0, width, height))
 		image = pygame.transform.scale(image, (width * scale, height * scale))
-		image.set_colorkey(colour)
 
 		return image
